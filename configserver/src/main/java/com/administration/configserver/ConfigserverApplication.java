f9f0757b2f879044afc593c87d4534b8655843df
chaopeng.lv <chaopeng.lv@freemud.cn>
2017-06-05 15:08:13 +0800

    configserver

--- configserver/src/main/java/com/administration/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/administration/configserver/ConfigserverApplication.java
@@ -1,9 +1,11 @@
-package com.administration.configserver.configserver;
+package com.administration.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {
